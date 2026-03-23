90717b0b6c9fb1a6be13983e75a748e6d21b575e
BLND-99 <blendmustafa71@gmail.com>
2026-03-23 00:08:00 -0400

    JavaFX displays the 34 tiles that the user will choose from (no functionality yet)

--- src/main/java/app/MainFX.java
+++ src/main/java/app/MainFX.java
@@ -3,10 +3,13 @@ package app;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import mahjong.Tile;
@@ -40,16 +43,32 @@ public class MainFX extends Application {
             Tile tile = wall.getTiles().get(i);
 
             // Create rectangle for the tile
-            Rectangle rect = new Rectangle(40, 60); // width 40, height 60
-            rect.setFill(Color.BEIGE);
-            rect.setStroke(Color.BLACK);
+            Rectangle front = new Rectangle(40, 40.0*(4.0/3)); // width 40, height 60
+            Rectangle rect = new Rectangle(40, 40.0*(4.0/3)); // width 40, height 60
+            // load from resources folder on classpath
+
+            String filename = tile.isRed() ? tile.getSortingValue() + "red.png" : tile.getSortingValue() + ".png";
+            Image tileImage = new Image(getClass().getResource("/tiles/" + filename).toExternalForm());
+            ImageView iv = new ImageView(tileImage);
+            iv.setFitWidth(100);     // width you want
+            iv.setFitHeight(150);    // height you want
+            iv.setPreserveRatio(true);
+            iv.setSmooth(true);      // smooth scaling
+            root.getChildren().add(iv);
+            Image tileImageFront = new Image(getClass().getResource("/tiles/Front.png").toExternalForm());
+            ImageView iv1 = new ImageView(tileImageFront);
+            iv1.setFitWidth(100);     // width you want
+            iv1.setFitHeight(150);    // height you want
+            iv1.setPreserveRatio(true);
+            iv1.setSmooth(true);      // smooth scaling
+            root.getChildren().add(iv1);
 
             // Label with tile info
             Label label = new Label(""+ tile.getSortingValue()); // make sure Tile.toString() shows suit+rank
             label.setStyle("-fx-font-size: 12; -fx-font-weight: bold;");
 
             // StackPane to overlay label on rectangle
-            StackPane stack = new StackPane(rect, label);
+            StackPane stack = new StackPane(iv1, iv);
 
             // Add to appropriate row
             if (i < 17) {
